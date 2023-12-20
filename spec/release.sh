3045c57560f51ccb31ceca6583e63ab203f817f7
Raimundo Henriques <raimundo.henriques@gmail.com>
2023-12-20 18:50:46 +0000

    feat: replace plausible site for releases

--- spec/release.sh
+++ spec/release.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 
-cd spec/v2
 pip3 install bikeshed && bikeshed update
+sed -i '' 's@wbcsd.github.io/data-exchange-protocol/v2@wbcsd.github.io/tr/data-exchange-protocol@g' v2/header.include
 make
 mkdir -p ../release/diagrams
 
-for i in diagrams/*.svg; do
+for i in v2/diagrams/*.svg; do
     cp $i ../release/diagrams/
 done
 
-cp index.html ../release/
+cp v2/index.html ../release/
