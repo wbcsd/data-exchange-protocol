2a8ed91ae90f048f54cde72f018318d1325ec308
Gertjan <gertjan@snok.nl>
2025-03-06 00:55:13 +0100

    docs: added RequestRejected documentation

--- tasks.py
+++ tasks.py
@@ -188,6 +188,9 @@ def experiment(c):
     """
     # scripts.openapi.test("spec/v3/openapi.yaml")
     # scripts.schema.validate_json_data("spec/v3/openapi.yaml", "spec/v3/examples/list-footprints-response.json")
+    # schexma = scripts.schema.load_openapi_file("spec/v3/openapi.yaml")
+    # schema = scripts.schema.navigate_to(schema, "components/schemas/RequestCreatedEvent")
+    # print(scripts.schema.dump_schema(schema))
     scripts.schema.validate_json_data(
         "spec/v3/openapi.yaml#paths/%2F3%2Ffootprints/get/responses/200/content/application%2Fjson/schema", 
         "spec/v3/examples/list-footprints-response.json"
